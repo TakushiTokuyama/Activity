a93292c9053c98090983cbb299a74d141a9bdc11
TakushiTokuyama <sokoninoruna@gmail.com>
2021-06-08 23:26:12 +0900

    メニューバーの変更処理をレンダラープロセスではなくメインプロセス側で行いたいため削除

--- src/linkSetting.js
+++ src/linkSetting.js
@@ -1,7 +1,3 @@
-// 現在のwindowを呼び出す
-const currentWindow = window.remote.getCurrentWindow();
-const Menu = window.remote.Menu;
-
 const setLinkSubmit = document.getElementById('setLinkSubmit');
 
 const linkNameElements = document.getElementsByClassName('linkName');
@@ -13,39 +9,6 @@ setLinkSubmit.addEventListener('click', function () {
     let linkNameValues = setArrayValues(linkNameElements);
     let linkUrlValues = setArrayValues(linkUrlElements);
 
-    let menus = new window.remote.Menu();
-
-    const links = new window.remote.MenuItem(
-        {
-            label: 'Link',
-            submenu: [
-                {
-                    label: 'Top',
-                    click() { currentWindow.loadFile('./view/index.html'); }
-                },
-                {
-                    label: 'GitHub',
-                    click() { currentWindow.loadURL('https://github.com/'); }
-                },
-                {
-                    label: 'HatenaBlog',
-                    click() { currentWindow.loadURL('https://tokuty.hatenablog.com/'); }
-                }
-            ]
-        },
-    )
-
-    const linkSetting = new window.remote.MenuItem(
-        {
-            label: 'LinkSetting',
-            click() { currentWindow.loadFile('./view/index.html'); }
-        },
-    )
-
-    menus.append(links);
-    menus.append(linkSetting);
-
-    Menu.setApplicationMenu(menus)
 }, false);
 
 // Elementの値を配列に格納する処理

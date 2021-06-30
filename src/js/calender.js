2c9b0c8a2b9287e8a4e1b514ad57b770458c03a8
TakushiTokuyama <sokoninoruna@gmail.com>
2021-06-30 23:23:17 +0900

    calender.js 追加

---
+++ src/js/calender.js
@@ -0,0 +1,43 @@
+// calender作成
+function createCalender(today, addMonth) {
+    // 月初と月末を返却する
+    var [beginingOfTheMonth, lastOfTheMonth] = returnFirstDayAndlastDay(today, addMonth);
+
+    // 月初の曜日
+    let beginingOfTheMonthDayOfTheWeek = beginingOfTheMonth.getDay();
+    // 月末日
+    let lastOfTheMonthDay = lastOfTheMonth.getDate();
+
+    console.log(lastOfTheMonth);
+
+    // 1か月分の日にち
+    let weeks = new Array(42);
+    // 日数を格納
+    for (var i = 1; i < lastOfTheMonthDay + 1; i++) {
+        weeks[beginingOfTheMonthDayOfTheWeek - 1 + i] = i;
+    }
+    return weeks;
+}
+
+// 月初と月末を返却する
+function returnFirstDayAndlastDay(today, addMonth) {
+    let currentYear = today.getFullYear();
+    let currentMonth = today.getMonth();
+    return [new Date(currentYear, currentMonth + addMonth, 1), new Date(currentYear, currentMonth + 1 + addMonth, 0)];
+}
+
+// １週ごとに分割
+function splitWeeks(weeks) {
+    let weekly = [];
+    let week = [];
+
+    // 6weekに分割
+    for (var x = 0; x < weeks.length; x++) {
+        week.push(weeks[x]);
+        if (week.length % 7 === 0) {
+            weekly.push(week);
+            week = [];
+        }
+    }
+    return weekly;
+}
